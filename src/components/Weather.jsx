dfc6cd58bf945a7193fc410f6ead907caa55af49
behzadk2 <bkazemi1994@gmail.com>
2024-08-05 20:20:50 +0200

    add Date function

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,17 +10,28 @@ function Weather() {
         } catch (error) {
             
         };
-
+        
         useEffect(()=>{
 
             fetchWeatherData();
         },[])
     }
 
+    const currentDate = new Date();
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+      ];
+     
+      const month = months[currentDate.getMonth()];
+      const day = currentDate.getDate();
+      const year = currentDate.getFullYear();
+      
+      const formattedDate = `${month} ${day}, ${year}`;
 
   return (
     <div className='container'>
-        <p>date</p>
+        <p>{formattedDate}</p>
         <h1>CityName</h1>
         <img src="" alt="logoweather" />
         <h1>temprature</h1>
